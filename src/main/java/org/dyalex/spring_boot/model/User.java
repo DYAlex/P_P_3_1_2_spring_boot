394719adcf38247272decbdc462b04228215c238
DYAlex <yvette.demidova@yandex.ru>
2024-01-30 16:48:26 +0300

    Added User model

---
+++ src/main/java/org/dyalex/spring_boot/model/User.java
@@ -0,0 +1,31 @@
+package org.dyalex.spring_boot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "role")
+    private String role;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String role) {
+        this.name = name;
+        this.lastName = lastName;
+        this.role = role;
+    }
+}
