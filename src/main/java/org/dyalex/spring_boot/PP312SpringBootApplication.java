b1a709c0711acb885f68f35664b720b2963639cb
DYAlex <yvette.demidova@yandex.ru>
2024-01-30 13:16:20 +0300

    Init commit

---
+++ src/main/java/org/dyalex/spring_boot/PP312SpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.dyalex.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PP312SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PP312SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dyalex/spring_boot/PP312SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dyalex.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PP312SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
