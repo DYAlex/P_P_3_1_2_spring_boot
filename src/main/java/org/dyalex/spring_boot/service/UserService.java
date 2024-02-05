38d95a6af4275b965cc12364abf2b9a469200635
DYAlex <yvette.demidova@yandex.ru>
2024-02-05 10:33:27 +0300

    Added UserService interface

--- src/main/java/org/dyalex/spring_boot/service/UserService.java
+++ src/main/java/org/dyalex/spring_boot/service/UserService.java
@@ -1,37 +1,12 @@
 package org.dyalex.spring_boot.service;
 
 import org.dyalex.spring_boot.model.User;
-import org.dyalex.spring_boot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public User findById(Long id) {
-        return userRepository.getOne(id);
-    }
-
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    @Transactional
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    @Transactional
-    public void deleteById(Long id) {
-        userRepository.deleteById(id);
-    }
+public interface UserService {
+    User findById(Long id);
+    List<User> findAll();
+    User saveUser(User user);
+    void deleteById(Long id);
 }
---
+++ src/main/java/org/dyalex/spring_boot/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package org.dyalex.spring_boot.service;
+
+import org.dyalex.spring_boot.model.User;
+import org.dyalex.spring_boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final User defaultUser = new User("Noname", "Nonamer", "No role");
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(defaultUser);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
